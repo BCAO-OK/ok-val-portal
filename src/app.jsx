761ab53ef416be4984d4eba9198a3ab4cba3b07a
BCAO-OK <assessor@bcap.info>
2026-02-21 15:56:33 -0600

    index and app update

--- src/app.jsx
+++ src/app.jsx
@@ -236,3 +236,14 @@ const App = () => {
 };
 
 export default App;
+
+import ReactDOM from 'react-dom/client';
+
+// ... all your existing component code ...
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
